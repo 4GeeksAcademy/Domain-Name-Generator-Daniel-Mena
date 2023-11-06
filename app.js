23d24cfeed86468c8ec351b14edd751adcef7fe9
DANNIMENNA89 <146239929+DANNIMENNA89@users.noreply.github.com>
2023-11-06 19:53:48 +0000

    Cambios de orden segun enunciado

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let noun = ['jogger', 'racoon'];
 for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
         for (let k = 0; k < noun.length; k++) {
-            console.log(pronoun[i]+adj[j]+noun[k]+".com")    
+            console.log(pronoun[j]+adj[i]+noun[k]+".com")    
         }
     }    
 }
